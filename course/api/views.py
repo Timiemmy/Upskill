100f5563ed5952fb587646b970487d7d46500e9d
Timiemmy <faith.bolanle@yahoo.com>
2025-02-20 15:52:19 +0100

    Made addition and modifications to the API

--- course/api/views.py
+++ course/api/views.py
@@ -1,12 +1,38 @@
-from rest_framework import generics
 from django.db.models import Count
-from course.api.serializers import SubjectSerializer
-from course.models import Subject
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from course.api.serializers import CourseSerializer, SubjectSerializer
+from course.api.paginations import StandardPagination
+from course.models import Course, Subject
 
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.prefetch_related('modules')
+    serializer_class = CourseSerializer
+    pagination_class = StandardPagination
+
+    @action(detail=True,methods=['post'],authentication_classes=[BasicAuthentication],permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
+
+
+class SubjectViewsets(viewsets.ReadOnlyModelViewSet):
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
+    serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
 
 class SubjectListView(generics.ListAPIView):
-    queryset = Subject.objects.annotate(total_course = Count('courses'))
+    queryset = Subject.objects.annotate(total_courses = Count('courses'))
     serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
 
 
 class SubjectDetailView(generics.RetrieveAPIView):
@@ -14,3 +40,13 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 
+# This has been added to courseviewsets as an action.
+'''
+class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    def post(self, request, pk, format=None)
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
+'''
